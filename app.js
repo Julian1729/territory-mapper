3ccec4b0ecd5c69721c14f0feb528f95709b0f51
Julian1729 <hernandez.julian17@gmail.com>
2019-11-07 15:51:33 -0500

    Remove hover quirk

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 18,
-    // zoom: 15,
-    // center: new google.maps.LatLng( 39.930315, -75.1586863 ),
     center: {lat: 39.930315, lng: -75.1586863},
     mapTypeId: 'roadmap'
   });
@@ -16,13 +14,6 @@ function initMap() {
     features.forEach(function(Feature){
       Feature.toGeoJson(function(obj){
         let coordinates = obj.geometry.coordinates;
-        // map to latLng objects
-
-        var lineSymbol = {
-          path: 'M 0,-1 0,1',
-          strokeOpacity: 1,
-          scale: 4
-        };
 
         let path = coordinates.map(latLng => {
           return new google.maps.LatLng({lat: latLng[1], lng: latLng[0]});
@@ -30,16 +21,10 @@ function initMap() {
         // draw lines between markers
         let block = new google.maps.Polyline({
           path,
-          geodesic: true,
+          // geodesic: true,
           strokeColor: '#FF0000',
-          strokeOpacity: 0,
-          strokeWeight: 4,
-          icons: [{
-            icon: lineSymbol,
-            offset: '0',
-            repeat: '30px'
-          }],
-          // map
+          strokeOpacity: 1,
+          strokeWeight: 10,
         });
 
         block.addListener('click', function(){
@@ -47,26 +32,21 @@ function initMap() {
         });
 
         block.addListener('mouseover', function(){
+
           DOM_CACHE.$infoBox.text(`${obj.properties.hundred} ${obj.properties.street} ${obj.properties.side} side`);
+
+        });
+
+        block.addListener('mouseout', function(){
+
+          DOM_CACHE.$infoBox.text('');
+
         });
 
         block.setMap(map);
-        
+
       });
     });
   });
 
 }
-
-// // Loop through the results array and place a marker for each
-// // set of coordinates.
-// window.eqfeed_callback = function(results) {
-//   for (var i = 0; i < results.features.length; i++) {
-//     var coords = results.features[i].geometry.coordinates;
-//     var latLng = new google.maps.LatLng(coords[1],coords[0]);
-//     var marker = new google.maps.Marker({
-//       position: latLng,
-//       map: map
-//     });
-//   }
-// }
